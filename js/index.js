220ce4deae6dfaae3b0c77a472899c385ec8e9e7
Eric Freitas <ericfreitas88@gmail.com>
2025-02-10 22:28:47 -0300

    Atualização para melhoria do responsivo e melhoria em como o menu hamburguer é exibido

--- js/index.js
+++ js/index.js
@@ -1,34 +1,12 @@
 $(document).ready(function () {
-  var existe_menu = document.getElementsByClassName("sub-menu");
-
-  // Ativa e desativa o Menu hamburguer
-  $(".icone-menu").on("click", function (e) {
-    e.preventDefault();
-
-    console.log(existe_menu.length);
-    var menu = "<div class='sub-menu'>" +
-      "<a class='sub-menu-item' href='#bateria'>Baterias</a>" +
-      "<a class='sub-menu-item' href='#services'>Serviços</a>" +
-      "<a class='sub-menu-item' href='#comentarios'>Depoimentos</a>" +
-      "<a class='sub-menu-item' href='#location'>Localização</a>" +
-      "</div>";
-
-    if (existe_menu.length == 0) {
-      $(".navg-menu-responsivo").append(menu)
-    } else {
-      $(".sub-menu").remove();
-    }
+  $(document).on("click", ".navg-menu-responsivo", function (e) {
+    e.stopPropagation();
+    $(this).find(".sub-menu").toggleClass("hidden");
   });
 
   $(document).on("click", function (e) {
-
-    console.log(e.target);
-    console.log($(e.target).hasClass("sub-menu-item"));
-
-    if ($(e.target).hasClass(".sub-menu-item")) {
-      console.log("submenu");
-      console.log(e.target);
-      
+    if (!$(this).closest(".navg-menu-responsivo").length) {
+      $(".navg-menu-responsivo").find(".sub-menu").addClass("hidden");
     }
-  })
-})
+  });
+});
